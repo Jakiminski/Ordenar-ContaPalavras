2619a125f5c9a16dae7c4338ca91db13acc10834
Jonas Jakiminski <28489507+Jakiminski@users.noreply.github.com>
2019-05-25 01:33:14 -0300

    START UP ENGAGE!
    Aquela referência que você pode não entender.

---
+++ main.py
@@ -0,0 +1,69 @@
+# Imports
+import sys ##argv, executable and exit([int or obj])
+from util import *
+
+if __name__ == '__main__' and len(sys.argv)>1:
+
+	i = 0
+	for arg in sys.argv: 
+		print('sys.argv[{}]: {}'.format(i,arg))
+		i += 1
+	print('len(sys.argv) = {}\n\n'.format(len(sys.argv)))
+
+	# I/O com Arquivos
+
+	#Entrada: Leitura de um arquivo de texto qualquer, contabilizando as palavras
+	fileInput = 'input/' + sys.argv[1].lower()
+	
+	#Saida: Escrever em arquivo, em ordem crescente, uma lista de ocorrências de cada palavra
+	fileOutput = 'output/' + sys.argv[1].lower()
+	
+	# Exemplo: 'poema1.txt' ou 'mysourcecode.txt'
+	print('Arquivo de entrada: {}\nArquivo de saída: {}'.format(fileInput,fileOutput))
+
+
+	# Selecionar modo
+	flag = True;
+	while (flag):
+		optionStr = str(input('Escolha como armazenar as palavras:\n'))
+		flag = options_check(optionStr)
+		options_show(flag)
+
+	# Executar modo escolhido (0/5 modos)
+	if optionStr.lower() == 'hash_lista':# Hash com lista-encadeada
+		# CHAMAR OBJETOS E SUBROTINAS ESPECÍFICAS(0/4)
+		#Criar Tabela
+		#Criar Lista
+		#Fazer a contagem e armazenar na tabela e/ou lista-encadeada
+		#Exibir Saída
+		print('\'{}\' really works!'.format(optionStr.lower()))
+	elif optionStr.lower() == 'hash_duplo':# Hash Duplo
+		# CHAMAR OBJETOS E SUBROTINAS ESPECÍFICAS(0/3)
+		#Criar Tabela
+		#Fazer a contagem e armazenar na tabela
+		#Exibir Saida
+		print('\'{}\' really works!'.format(optionStr.lower()))
+	elif optionStr.lower() == 'avl':# Árvore AVL
+		# CHAMAR OBJETOS E SUBROTINAS ESPECÍFICAS(0/3)
+		#Criar Árvore AVL
+		#Fazer a contagem e armazenar na árvore
+		#Exibir Saída
+		print('\'{}\' really works!'.format(optionStr.lower()))
+	elif optionStr.lower() == 'redblack':# Árvore Rubro-Negra
+		# CHAMAR OBJETOS E SUBROTINAS ESPECÍFICAS(0/3)
+		#Criar Árvore Rubro-Negra
+		#Fazer a contagem e armazenar na árvore
+		#Exibir Saída
+		print('\'{}\' really works!'.format(optionStr.lower()))
+	elif optionStr.lower() == 'arvore_b':# Árvore B
+		# CHAMAR OBJETOS E SUBROTINAS ESPECÍFICAS(0/3)
+		#Criar Árvore B
+		#Fazer a contagem e armazenar na árvore
+		#Exibir Saída
+		print('\'{}\' really works!'.format(optionStr.lower()))
+	else:# Erro
+		sys.exit('ERRO: Não há Estrutura de Dados com o tipo informado. {}.'.format(optionStr))
+
+	sys.exit(0)
+
+# END MAIN
---
+++ util.py
@@ -0,0 +1,88 @@
+# Utilitarios (2/4)
+
+import sys #argv, executable and exit([int or obj])
+import random #seed(),randint(),randrange(),shuffle(obj)
+
+
+# 1.OPTIONS
+
+#1.1 Imprime opções de escolha da estrutura de dados
+def options_show(boo):
+		if boo: 
+			print('Insira:\n\'hash_lista\', \'hash_duplo\', \'avl\', \'redblack\' ou \'arvore_b\'.')
+		else:
+			print('Iniciando...')
+		pass
+
+#1.2 Checa Se imprime as opcoes ou continua loop
+#Retorna um boolean para controle de loop
+def options_check(str1):
+	if (str1.lower() == 'hash_lista') or (str1.lower() == 'hash_duplo'):
+		return False #parar o loop
+	elif (str1.lower() == 'avl') or (str1.lower() == 'redblack') or (str1.lower() == 'arvore_b'):
+		return False #parar o loop
+	else: 
+		return True # continuar o loop
+
+# 2.QUICKSORT
+
+'''
+QuickSort para ordem crescente.
+Args: altera o vetor que será ordenado
+Returns: O mesmo vetor, porém, ordenado
+
+Execução: Divisão(Dividir para conquistar)
+[NÃO ESTÁVEL] -- O(n log n) 
+	
+
+'''
+def quickSort(vet,inicio,fim):
+	# inicio e fim são índices da partição
+	if inicio<fim:
+		# PASSO BASE: método particiona
+		index = particionaAleatorio(vet,inicio,fim) # índice do pivô
+
+		# PASSO RECURSIVO
+		quickSort(vet,inicio,index-1) #elementos à esquerda do pivô
+		quickSort(vet,index+1,fim) #elementos à direita do pivô
+	return vet
+
+'''
+Particionamento, excluindo o pivô um nível acima da pilha de execução
+Args: vetor e índices de início e fim de cada partição
+Returns: O índice do pivô na lista/vetor
+'''
+def particionaFim(vet,inicio,fim):
+	i = inicio-1 #index do menor elemento
+	pivot = vet[fim] #pivô
+	for j in range(inicio,fim):
+		if vet[j]<=pivot:
+			i += 1
+			vet[i],vet[j] = vet[j],vet[i] #swap
+	vet[i+1],vet[fim] = vet[fim],vet[i+1] #swap
+	return i+1 #será índice do pivô no vetor
+
+def particionaAleatorio(vet,inicio,fim):
+	# Número aleatório DA PARTIÇÃO
+	random.seed()
+	i = random.randint(inicio,fim) #index do pivô (vet[i])
+	# trocar o pivô e o último elemento
+	vet[i],vet[fim] = vet[fim],vet[i]
+	# executar outro método de partição
+	return particionaFim(vet,inicio,fim)
+
+
+vetor = [5,9,6,4,4,2,0,4,3,2,6,1]# lista indexada
+
+print('Antes: ')
+print(vetor)
+print('Aplicando o QuickSort.')
+vetor = quickSort(vetor,0,len(vetor)-1) #arg está implícito
+print('Após: ')
+print(vetor)
+
+# 3.INPUT FILE
+
+# 4.OUTPUT FILE
+
+#END Utilitários
